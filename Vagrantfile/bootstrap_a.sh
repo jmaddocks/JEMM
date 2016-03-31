54ce4aa201c385f8c65a39d27c6f97f302abed02
Elliot5849 <elliot.marsh@qa.com>
2016-03-31 13:49:35 +0000

    Vagrant file and bootstrap script

---
+++ Vagrantfile/bootstrap_a.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt-get update 
+sudo apt-get install -y openssh-server openssh-client
+ifconfig
+sudo ufw disable
+
+if grep -aq "puppetmaster" /etc/hosts
+then # provision has already been run - update hosts
+
+	#update ip in hosts
+	sed -i "1s/.*/$1\tJEMMmaster.qac.local\tpuppetmaster/" /etc/hosts
+
+else # run provision for first time - install all	
+	
+	apt-get install -y openssh-server puppet
+
+	echo -e "$1\tJEMMmaster.qac.local\tpuppetmaster\n127.0.0.1\t$(facter fqdn)\tpuppet\n$(facter ipaddress_eth1)\t$(facter fqdn)\tpuppet" | cat - /etc/hosts > temp && sudo mv temp /etc/hosts
+
+	sed -i '1s/.*/[main]\nserver=JEMMmaster.qac.local/' /etc/puppet/puppet.conf
+	puppet agent --enable
+
+fi 
+
+puppet agent -tv
+exit 0
