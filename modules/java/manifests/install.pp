889845d16be187604d8cc7125c4777fe73ad7043
Elliot5849 <elliot.marsh@qa.com>
2016-04-13 18:05:23 +0100

    Update install.pp

--- modules/java/manifests/install.pp
+++ modules/java/manifests/install.pp
@@ -1,42 +1,33 @@
-class java::install {
-		#archive : find where file is 
-		#home : where the file will be 
-		#version : java version
-		
-		$java_archive = "java.tar.gz",
-		$java_home = "/usr/local/java",
-		$java_version = "jdk1.8.0_45/"
-		
-	Exec {
-		path => ["/bin", "/usr/bin", "/usr/sbin"] #path to place file
-	}
+class java::install (
+  $java_archive = 'java.tar.gz',
+  $java_home = '/usr/local/java',
+  $java_version = 'jdk1.8.0_45/')
 	
-	file {"${java_home}" :	
-		ensure => 'directory', #making java_home is the directory
-	}
-	
-	file {"${java_home}${java_archive}":
-		ensure  => 'present',
-		source  => "puppet:///modules/java/${java_archive}",
-		require => File ["${java_home}"],
-	}
+  {
+  Exec {
+  path => ['/bin', '/usr/bin', '/usr/sbin'] #path to place file
+  }
 
-	exec {"${extract_java}":
-		cwd 	=> "${java_home}",
-		command => "tar zxvf ${java_archive}",
-		creates => "${java_home}${java_version}",
-		require => File["${java_home}${java_archive}"],
-	}
-	
-	exec {"${install_java}":
-		require   => Exec["extract_java"],
-		logoutput => true,
-		command   => "update-alternatives --install /bin/java java ${java_home}${java_version}bin/java 100 && update-alternatives --install /bin/javac javac ${java_home}${java_version}bin/javac 100",
-	}
-	
-	exec {"${set_java}":
-		require   => Exec['install_java'],
-		logoutput => true,
-		command   => "update-alternatives --set java £{java_home}/bin/java"
-	}
+  file {"${java_home}" :
+  ensure => 'directory' #making java_home is the directory
+  }
+
+  file {"${java_home}${java_archive}":
+  ensure  => 'present',
+  source  => "puppet:///modules/java/${java_archive}",
+  require => File ["${java_home}"],
+  }
+
+  exec {'extract_java':
+  cwd 	  => '${java_home}',
+  command => "tar zxvf ${java_archive}",
+  creates => "${java_home}${java_version}",
+  require => File["${java_home}${java_archive}"],
+  }
+
+  exec {'install_java':
+  require   => Exec['extract_java'],
+  logoutput => true,
+  command   => "update-alternatives --install /bin/java java ${java_home}${java_version}bin/java 100 && update-alternatives --install /bin/javac javac ${java_home}${java_version}bin/javac 100",
+  }	
 }
