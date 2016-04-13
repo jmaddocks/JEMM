fec58d8ca0fd515f274a4dca3f9700d1a3a476b8
Elliot5849 <elliot.marsh@qa.com>
2016-04-13 17:59:13 +0100

    Update install.pp

--- modules/git/manifests/install.pp
+++ modules/git/manifests/install.pp
@@ -1,44 +1,36 @@
-class git::install {
-	#declaring variables to reduce repeating long winded file names
-	
-	$git_path     = "/usr/lib/git/", 		#where to save file
-	$git_version  = "git-2.8.1",		    #version of git to install
-	$git_tar      = "git-2.8.1.tar.gz", 	#git tarball
-	$git_packages = "[libcurl4-gnutls-dev","libexpat1-dev", "gettext", "libz-dev", "libssl-dev]",
-	
-	#git requres these packages to function properly:URL transfer library;XML parsing c library development kit;internationalise the language;virtual package;SSL development libraries
-	
-	)
+class git::install (
+  $git_path     = "/usr/lib/git/",
+  $git_version  = "git-2.8.1",
+  $packages = ["libcurl4-gnutls-dev","libexpat1-dev", "gettext", "libz-dev", "libssl-dev"])
+  {
+  Exec {
+  path => ['/bin','/usr/bin','/usr/sbin'] 
+  }
 
-	Exec {
-		path => ['/bin','/usr/bin','/usr/sbin'] #binaries required for code below to be recognised
-	}
+  package { $packages :
+  ensure => 'installed',
+  }
 
-	package { "${git_packages}":
-		ensure => 'present',					#package installed
-	}
-	
-	file { "${git_path"}:
-		ensure  => 'directory',
-		require => Package[$git_packages],		#make sure directory
-	}
-	
-	file { "${git_path}${git_tar}":
-		ensure  => 'present',
-		source  => "puppet:///modules/git/files/${git_tar}",
-		require => File["${git_path}"],			#ensuring the file exists if not find the source
-	}
-		
-	exec { "extract git":	#
-		cwd   	=> "${git_path}",
-		command => "tar zxvf ${git_tar}",
-		create  => "${git_path}${git_version}",
-		require => File["${git_path}${git_tar}"], 	#Extract java
-	}
-	
-	exec { "install git":
-		require => Exec["${extract git}"],
-		cwd  	=> "${git_path}${git_version}",
-		command => "make prefix=/usr/local all && make prefix=/usr/local install", 	#Install
-	}
+  file { "${git_path}"} :
+  ensure  => 'directory',
+  require => Package[[$packages]],		#make sure directory
+  }
+
+  file { "${git_path}git-2.8.1.tar.gz":
+  ensure  => 'present',
+  source  => "puppet:///modules/git/git-2.8.1.tar.gz",
+  require => File["${git_path}"],
+  }
+
+  exec { "extract git":
+  cwd     => "${git_path}",
+  command => "tar zxvf git-2.8.1.tar.gz",
+  require => File["${git_path}git-2.8.1.tar.gz"] 	
+  }
+
+  exec { "install git":
+  require => Exec["extract git"],
+  cwd  	  => "${git_path}${git_version}",
+  command => "make prefix=/usr/local all && make prefix=/usr/local install" 	#Install
+  }
 }
