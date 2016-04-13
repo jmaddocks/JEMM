a13dad7dafd9df4d29539dd12a7c9d5068e4c095
Elliot5849 <elliot.marsh@qa.com>
2016-04-13 17:57:41 +0100

    Update install.pp

--- modules/Jenkins/manifests/install.pp
+++ modules/Jenkins/manifests/install.pp
@@ -1,34 +1,38 @@
-class jenkins::install(
+class jenkins::install {
 
-//before doing this: deb http://pkg.jenkins-ci.org/debian binary/ is placed into files/../sources.list. This creates a source list for Jenkins
+  Exec {
+  path => ['/bin', '/usr/bin', '/usr/sbin', '/usr/local/sbin', '/sbin']
+  }
 
-//wget http://...key for package keys are put into the bootstrap for vagrantfile
-	
-	$jenkins_path = "/etc/apt/sources.list.d/jenkins.list": //source list for Jenkins for software files
-	
-)
-
-	Exec { 'install_jenkins_package_keys':class jenkins {
-	
-	exec { 'install_jenkins_package_keys':
-		command => '/usr/bin/wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | /usr/bin/apt-key add - ',
+  exec {'keys':
+  command => 'wget -q -O - http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/source.list.d/jenkins.list'",
+  require => Exec ['keys']
   }
 		
+  files { '/etc/apt/sources.list.d/jenkins.list',
+  mode => '0644',
+  owner => root,
+  group => root,
+  source => 'puppet:///modules/jenkins/jenkins.list'
+  }
 
-	file { "${jenkins_path}":
-		mode => 644
-		owner => root,
-		group => root,
-		source => "puppet:///modules${jenkins_path}",
-	}
-	
-	package { 'jenkins':
-		ensure => latest,
-		require => File ["${install_jenkins_package_keys}${jenkins_path}"],
-	}
-	
-	service { "jenkins":
-		ensure => running,
-	}
-}
+  exec { 'update':
+  command => 'sudo apt-get update'
+  require => File['/etc/apt.sources.list.d/jenkins.list']
+  }
+
+  exec { 'install':
+  command => 'sudo apt-get install -y jenkins',
+  require => Exec['update']
+  }
+  
+  exec{ 'stop':
+  command => 'sudo service jenkins stop'
+  require => Exec['install']
+  }
+
+  exec { 'start':
+  command => 'sudo service jenkins start'
+  require => Exec['stop']
+  }
 }
